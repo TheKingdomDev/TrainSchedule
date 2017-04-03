e678ae3bb67a1f724d5bae50b53bf360c0adbd41
Eric King <Eric King>
2017-04-03 13:13:00 -0400

    html/bootstrap done

---
+++ assets/javascript/logic.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyByiRSEQtylEWkVQTh329mifF-JDlVwwAg",
+    authDomain: "train-schedule-89d60.firebaseapp.com",
+    databaseURL: "https://train-schedule-89d60.firebaseio.com",
+    projectId: "train-schedule-89d60",
+    storageBucket: "train-schedule-89d60.appspot.com",
+    messagingSenderId: "743286417177"
+  };
+  firebase.initializeApp(config);
